d848237d4ed32b47bd802f0e7fef703deb16a22b
rohit <rohitchandel2023@gmail.com>
2025-03-26 23:57:18 +0530

    bookking logic

--- src/Views/Contact/Contact.tsx
+++ src/Views/Contact/Contact.tsx
@@ -1,7 +1,10 @@
+import CalendarComponent from "../Shared/CalendarComponent";
+
 function ContactPage(){
     return(
         <>
     <h2>this is contact page</h2>
+    <CalendarComponent />
         </>
     )
 }
